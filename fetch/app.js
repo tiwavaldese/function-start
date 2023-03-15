3841b30909b9726459d250d6da0f1395d447f2f3
tiwavaldese <tiwaval7@gmail.com>
2023-03-15 09:22:25 +0100

    fetch/

--- fetch/app.js
+++ fetch/app.js
@@ -1,20 +1,9 @@
 alert
 
 
-const fs = require('fs');
-fs.readFile('test.txt','utf8',(err,data)=> {
-    if(err){
-        console.error(err);
-        return;
-    }
-    console.log(data);
-});
-
-
-
 
 const content = "something interesting";
-fs.writeFile('test.txt', content,err => {
+fs.appendFile('test.txt', content,err => {
     if(err){
         console.err;
         return;
